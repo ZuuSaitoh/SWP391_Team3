4c931aee71dbfdbc4d4dcb99c96ec6ae3416a53e
Phatdvt <phatdvtse183465@fpt.edu.vn>
2024-09-25 00:15:41 +0700

    Testn

---
+++ hello-world-spring-boot/src/main/java/com/devteria/hello_world_spring_boot/controller/helloWorldController.java
@@ -0,0 +1,12 @@
+package com.devteria.hello_world_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+    @GetMapping("/hello")
+    String hello(){
+        return "Hello spring boot 3 I'm good :)))";
+    }
+}
