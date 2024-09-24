b6473a3cb640ca10a5503346dda4a87da731c20a
ZuuSaitoh <dokydungtnt1072004@gmail.com>
2024-09-25 00:24:49 +0700

    del

--- website/koi-pond-building/src/page/home/index.jsx
+++ website/koi-pond-building/src/page/home/index.jsx
@@ -1,13 +1,5 @@
 
 
 const Home = () => {
-   //Hello World 
-   //Cai nay la trang home 
-   //Tui se lam trang home
-
-
-   //hello Dung senpai
-   //hello Dung senpai dep trai vippro
-
-   //hello lam senpai
+   
 };
