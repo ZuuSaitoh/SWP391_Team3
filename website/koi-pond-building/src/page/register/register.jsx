2a305ace3adc3a25bc8b5e276274f29cb12cb423
ZuuSaitoh <dokydungtnt1072004@gmail.com>
2024-09-27 08:25:32 +0700

    h2 img

--- website/koi-pond-building/src/page/register/register.jsx
+++ website/koi-pond-building/src/page/register/register.jsx
@@ -10,39 +10,53 @@ const Register = () => {
   };
 
   return (
-    <div className='container'>
-      <div className='header'>
-        <div className='text'>Sign Up</div>
-        <div className='underline'></div>
+    <div className='register-container'>
+      {/* Left side with image */}
+      <div className='image-container'></div>
+
+      {/* Right side with form */}
+      <div className='form-container'>
+        <div className='header'>
+          <div className='text'>Sign Up</div>
+          <div className='underline'></div>
+        </div>
+        <div className='inputs'>
+          <div className='input'>
+            <input type='text' placeholder='Username' />
+          </div>
+          <div className='input'>
+            <input type='email' placeholder='Email' />
+          </div>
+          <div className='input'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Password'
+            />
+          </div>
+          <div className='input'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Confirm Password'
+            />
+          </div>
+          <div className='show-password'>
+            <input
+              type='checkbox'
+              id='showPassword'
+              checked={showPassword}
+              onChange={togglePasswordVisibility}
+            />
+            <label htmlFor='showPassword'>Show Password</label>
+          </div>
+        </div>
+
+        <div className='submit-container'>
+          <div className='submit'>Sign Up</div>
+        </div>
+        <div className='already-haveAccount'>
+          Already have an Account? <span>Click here</span>
+        </div>
       </div>
-      <div className='inputs'>
-        <div className='input'>
-          <input type='text' placeholder='Username' />
-        </div>
-        <div className='input'>
-          <input type='email' placeholder='Email' />
-        </div>
-        <div className='input'>
-          <input type={showPassword ? 'text' : 'password'} placeholder='Password' />
-        </div>
-        <div className='input'>
-          <input type={showPassword ? 'text' : 'password'} placeholder='Confirm Password' />
-        </div>
-        <div className="show-password">
-          <input 
-            type="checkbox" 
-            id="showPassword" 
-            checked={showPassword} 
-            onChange={togglePasswordVisibility} 
-          />
-          <label htmlFor="showPassword">Show Password</label>
-        </div>
-      </div>
-      
-      <div className="submit-container">
-        <div className="submit">Sign Up</div>
-      </div>
-      <div className="already-haveAccount">Already have an Account? <span>Click here</span></div>
     </div>
   );
 };
