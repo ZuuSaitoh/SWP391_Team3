13b638f0e38080787c2e674dc2a4820bd9aa5509
ZuuSaitoh <dokydungtnt1072004@gmail.com>
2024-10-02 12:03:09 +0700

    import useState for forgot password

--- website/koi-pond-building/src/page/forgotpassword/forgotpassword.jsx
+++ website/koi-pond-building/src/page/forgotpassword/forgotpassword.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./forgotpassword.css";
 import { useNavigate } from "react-router-dom";
 import AnimatedPage from "../animationpage/AnimatedPage";
