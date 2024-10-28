b95edaed8dc164264e45d4fdaf0e65a23a853ef7
ZuuSaitoh <154410363+ZuuSaitoh@users.noreply.github.com>
2024-10-28 12:10:24 +0700

    Delete swp391 directory

--- swp391/src/main/java/swp391/com/swp391/repository/DiscountRepository.java
+++
@@ -1,9 +0,0 @@
-package swp391.com.swp391.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import swp391.com.swp391.entity.Discount;
-
-@Repository
-public interface DiscountRepository extends JpaRepository<Discount, Integer> {
-}
